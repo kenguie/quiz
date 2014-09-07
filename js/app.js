37ada0346f0383811f927b1d22f875be30428ea0
Ken <nyhunter77@yahoo.com>
2014-09-06 21:46:51 -0400

    updating

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+  // Array holding questions and answers
+  var questions = [{
+        question: "Who is the leader of the Autobots?",
+        choices: ["Gears", "Arcee", "Hoist", "Optimus Prime"],
+        qNum : 0,
+        correct : 3,
+        fact: "Optimus Prime is the leader of the Autobots."
+        },
+        {
+        question: "Who is the leader of the Decepticons?",
+        choices: ["Starscream", "Megatron", "Scorpinox", "Astrotrain"],
+        qNum : 1,
+        correct : 1,
+        fact: "Megatron is the supreme leader of the Decepticons (even after his reformat in 2005)."
+        },
+        {
+        question: "After reformatting by Unicron, who does Megatron become?",
+        choices: ["Galvatron", "Metroplex", "Cybertron", "Smokescreen"],
+        qNum : 2,
+        correct : 0,
+        fact: "After Unicron reformats Megatron's battered body, he is from there, known as Galvatron."
+        },
+        {
+        question: "Who is the young Transformer who became the new Autobot leader after Optimus Prime's death?",
+        choices: ["Springer", "Kup", "Hot Rod", "Wreck-Gar"],
+        qNum : 3,
+        correct : 2,
+        fact: "Hot Rod is the young Autobot thrust into leadership after opening the Matrix of Leadership inside the belly of Unicron."
+        },
+        {
+        question: "Who are the 2 brothers shown in images on this webpage?",
+        choices: ["Sunstreaker and Sideswipe", "Prowl and Jazz", "Ironhide and Rachet", "Wheeljack and Windcharger"],
+        qNum : 4,
+        correct : 0,
+        fact: "The 2 Lamborghini brothers seen on this page are the yellow Sunstreaker and red Sideswipe."
+    }];
+
+  var questionNumber = 0;
+  var userScore = 0;
+  
+});
